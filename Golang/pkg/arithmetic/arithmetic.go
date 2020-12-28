a74e06c4f83691147fd210f3d979abe105c748c2
Konstantinos Railis <krailis@hotmail.com>
2020-12-28 23:42:45 +0200

    Finalized Arithmetic Solutions in Golang.

--- Golang/pkg/arithmetic/arithmetic.go
+++ Golang/pkg/arithmetic/arithmetic.go
@@ -1,67 +1,214 @@
+// This package includes solutions to Arithmetic problems (31-41).
 package arithmetic
 
 import (
-  "errors"
-  "lists"
-  "math"
-  "gopkg.in/guregu/null.v3"
+	"errors"
+	"fmt"
+	"gopkg.in/guregu/null.v3"
+	"math"
+	"math/rand"
+	"time"
 )
 
 type IntSlice []int
+type ISlice []interface{}
 
+// IsPrime determines whether a given number is prime (P31).
 func IsPrime(number int) bool {
-  upperDivisor := int(math.Sqrt(float64(number))) + 1
-  for divisor := 2; divisor < upperDivisor ; divisor++ {
-    if number % divisor == 0 {
-      return false
-    }
-  }
-  return true
+	upperDivisor := int(math.Sqrt(float64(number))) + 1
+	for divisor := 2; divisor < upperDivisor; divisor++ {
+		if number%divisor == 0 {
+			return false
+		}
+	}
+	return true
 }
 
+// Gcd determines the greatest common divisor of two positive integer numbers (P32).
 func Gcd(a int, b int) (int, error) {
-  if a < 0 || b < 0 {
-    return -1, errors.New("The given numbers are not positive.")
-  }
+	if a < 0 || b < 0 {
+		return -1, errors.New("The given numbers are not positive.")
+	}
 
-  // Euclid's Algorithm
-  if b == 0 {
-    return a, nil
-  } else {
-    return Gcd(b, a % b)
-  }
+	// Euclid's Algorithm
+	if b == 0 {
+		return a, nil
+	} else {
+		return Gcd(b, a%b)
+	}
 }
 
+// IsCoprime determines if two positive integers are coprime (P33).
 func IsCoprime(a int, b int) (null.Bool, error) {
-  if a < 0 || b < 0 {
-    return null.BoolFromPtr(nil), errors.New("The given numbers are not positive.")
-  }
+	if a < 0 || b < 0 {
+		return null.BoolFromPtr(nil), errors.New("The given numbers are not positive.")
+	}
 
-  gcd, _ := Gcd(a, b)
-  return null.BoolFrom(gcd == 1), nil
+	gcd, _ := Gcd(a, b)
+	return null.BoolFrom(gcd == 1), nil
 }
 
+// TotientPhi calculates Euler's totient function phi(m) (P34).
+func TotientPhi(m int) (int, error) {
+	if m < 1 {
+		return -1, errors.New("The given number is not positive")
+	}
+
+	if m == 1 {
+		return 1, nil
+	}
+	if IsPrime(m) {
+		return m - 1, nil
+	}
+	coPrimes := 1
+	for i := 2; i < int(m/2)+1; i++ {
+		if m%i != 0 {
+			if coPrime, _ := IsCoprime(i, m); coPrime.ValueOrZero() {
+				coPrimes++
+			}
+		}
+	}
+	for i := int(m/2) + 1; i < m; i++ {
+		if coPrime, _ := IsCoprime(i, m); coPrime.ValueOrZero() {
+			coPrimes++
+		}
+	}
+	return coPrimes, nil
+}
+
+// PrimeFactors determine the prime factors of a given positive number (P35).
 func PrimeFactors(number int) IntSlice {
-  if IsPrime(number) {
-    return IntSlice{number}
-  }
-  primeFactors := IntSlice{}
-  for number % 2 == 0 {
-    primeFactors = append(primeFactors, 2)
-    number = int(number / 2)
-  }
-  for i := 3; i < int(math.Sqrt(float64(number))) + 1; i += 2 {
-    for number % i == 0 {
-      primeFactors = append(primeFactors, i)
-      number = int(number / i)
-    }
-  }
-  if number > 2 {
-    primeFactors = append(primeFactors, number)
-  }
-  return primeFactors
+	if IsPrime(number) {
+		return IntSlice{number}
+	}
+	primeFactors := IntSlice{}
+	for number%2 == 0 {
+		primeFactors = append(primeFactors, 2)
+		number = int(number / 2)
+	}
+	for i := 3; i < int(math.Sqrt(float64(number)))+1; i += 2 {
+		for number%i == 0 {
+			primeFactors = append(primeFactors, i)
+			number = int(number / i)
+		}
+	}
+	if number > 2 {
+		primeFactors = append(primeFactors, number)
+	}
+	return primeFactors
 }
 
-func PrimeFactorsMult(number int) []interface{} {
-  return lists.Encode(PrimeFactors(number))
+// PrimeFactorsMult returns the length-encoded prime factor list (P36).
+func PrimeFactorsMult(number int) []IntSlice {
+	primeFactors := PrimeFactors(number)
+	encoded, current, count := []IntSlice{}, primeFactors[0], 1
+	for _, factor := range primeFactors[1:] {
+		if current != factor {
+			encoded = append(encoded, IntSlice{count, current})
+			current, count = factor, 1
+			continue
+		}
+		count++
+	}
+	encoded = append(encoded, IntSlice{count, current})
+	return encoded
+}
+
+// TotientPhi2 calculates Euler's totient function effectively (P37).
+func TotientPhi2(m int) (int, error) {
+	if m < 1 {
+		return -1, errors.New("The given number is not positive")
+	}
+
+	if m == 1 {
+		return 1, nil
+	}
+	phi := 1.0
+	primeFactorsEncoded := PrimeFactorsMult(m)
+	for _, elem := range primeFactorsEncoded {
+		m, p := elem[0], elem[1]
+		phi *= float64(p-1) * math.Pow(float64(p), float64(m-1))
+	}
+	return int(phi), nil
+}
+
+// CompareTotient compare the two methods of calculating Euler's totient function (P38).
+func CompareTotient(numberOfTests int) {
+	rand.Seed(time.Now().UnixNano())
+	m := 0
+	fmt.Println("--------------------------------------------")
+	fmt.Printf("%s\t%10s\t%10s\n", "Argument", "TotientPhi", "TotientPhi2")
+	fmt.Println("============================================")
+	for i := 0; i < numberOfTests; i++ {
+		m = rand.Intn(20000)
+		// TotientPhi
+		startTotientPhi := time.Now()
+		TotientPhi(m)
+		elapsedTotientPhi := time.Since(startTotientPhi)
+		// TotientPhi2
+		startTotientPhi2 := time.Now()
+		TotientPhi2(m)
+		elapsedTotientPhi2 := time.Since(startTotientPhi2)
+		// Print time
+		fmt.Printf("%6v\t\t%10v\t%11v\n", m, elapsedTotientPhi, elapsedTotientPhi2)
+	}
+	fmt.Println("--------------------------------------------")
+}
+
+// PrimeList returns a list of prime numbers (P39).
+func PrimeList(lower int, upper int) IntSlice {
+	// Sieve of Eratosthenes.
+	array := make([]int, upper+1)
+	for i := 2; i < int(math.Sqrt(float64(upper)))+1; i++ {
+		if array[i] == 0 {
+			j := i * i
+			for j <= upper {
+				array[j] = 1
+				j += i
+			}
+		}
+	}
+	primeList := IntSlice{}
+	for i := lower - 1; i < upper+1; i++ {
+		if array[i] == 0 {
+			primeList = append(primeList, i)
+		}
+	}
+	return primeList
+}
+
+// Goldbach computes Goldbach's Conjecture (P40).
+func Goldbach(number int) (IntSlice, error) {
+	if number <= 2 {
+		return nil, errors.New("The given number is smaller than 2.")
+	}
+	if number%2 == 1 {
+		return nil, errors.New("The given number is odd.")
+	}
+
+	var prime int
+	primeList := PrimeList(3, int(number/2))
+	for _, prime = range primeList {
+		if IsPrime(number - prime) {
+			break
+		}
+	}
+	return IntSlice{prime, number - prime}, nil
+}
+
+// GoldbachList returns a list of Goldbach compositions (P41).
+func GoldbachList(lower int, upper int) ([]IntSlice, error) {
+	goldbachList := []IntSlice{}
+	if lower%2 == 1 {
+		lower += 1
+	}
+	for i := lower; i < upper+1; i = i + 2 {
+		numbers, err := Goldbach(i)
+		if err != nil {
+			return nil, err
+		} else {
+			goldbachList = append(goldbachList, numbers)
+		}
+	}
+	return goldbachList, nil
 }
--- Golang/pkg/arithmetic/arithmetic_test.go
+++ Golang/pkg/arithmetic/arithmetic_test.go
@@ -1,104 +1,296 @@
 package arithmetic
 
 import (
-  "fmt"
-  "reflect"
-  "testing"
-  "gopkg.in/guregu/null.v3"
+	"fmt"
+	"gopkg.in/guregu/null.v3"
+	"reflect"
+	"testing"
 )
 
 // TestIsPrime tests the solution of P31.
 func TestIsPrime(t *testing.T) {
-  var testCases = []struct {
-    x int
-    y bool
-  }{
-    {7, true},
-    {31, true},
-    {101, true},
-    {49, false},
-    {122, false},
-  }
+	var testCases = []struct {
+		x int
+		y bool
+	}{
+		{7, true},
+		{31, true},
+		{101, true},
+		{49, false},
+		{122, false},
+	}
 
-  for _, testCase := range testCases {
-    testName := fmt.Sprint(testCase.x)
-    t.Run(testName, func(t *testing.T) {
-      ans := IsPrime(testCase.x)
-      if ans != testCase.y {
-        t.Errorf("Expected %v but the result was %v", testCase.y, ans)
-      }
-    })
-  }
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.x)
+		t.Run(testName, func(t *testing.T) {
+			ans := IsPrime(testCase.x)
+			if ans != testCase.y {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
 }
 
 // TestGcd tests the solution of P32.
 func TestGcd(t *testing.T) {
-  var testCases = []struct {
-    a int
-    b int
-    y int
-  }{
-    {36, 63, 9},
-    {78, 32, 2},
-    {35, 49, 7},
-    {32, 112, 16},
-    {33, 132, 33},
-  }
+	var testCases = []struct {
+		a int
+		b int
+		y int
+	}{
+		{36, 63, 9},
+		{78, 32, 2},
+		{35, 49, 7},
+		{32, 112, 16},
+		{33, 132, 33},
+	}
 
-  for _, testCase := range testCases {
-    testName := fmt.Sprint(testCase.a, testCase.b)
-    t.Run(testName, func(t *testing.T) {
-      ans, _ := Gcd(testCase.a, testCase.b)
-      if ans != testCase.y {
-        t.Errorf("Expected %v but the result was %v", testCase.y, ans)
-      }
-    })
-  }
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.a, testCase.b)
+		t.Run(testName, func(t *testing.T) {
+			ans, _ := Gcd(testCase.a, testCase.b)
+			if ans != testCase.y {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
 }
 
 // TestIsCoprime tests the solution of P33.
 func TestIsCoprime(t *testing.T) {
-  var testCases = []struct {
-    a int
-    b int
-    y null.Bool
-  }{
-    {35, 64, null.BoolFrom(true)},
-    {35, 49, null.BoolFrom(false)},
-    {35, 56, null.BoolFrom(false)},
-  }
+	var testCases = []struct {
+		a int
+		b int
+		y null.Bool
+	}{
+		{35, 64, null.BoolFrom(true)},
+		{35, 49, null.BoolFrom(false)},
+		{35, 56, null.BoolFrom(false)},
+	}
 
-  for _, testCase := range testCases {
-    testName := fmt.Sprint(testCase.a, testCase.b)
-    t.Run(testName, func(t *testing.T) {
-      ans, _ := IsCoprime(testCase.a, testCase.b)
-      if ans != testCase.y {
-        t.Errorf("Expected %v but the result was %v", testCase.y, ans)
-      }
-    })
-  }
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.a, testCase.b)
+		t.Run(testName, func(t *testing.T) {
+			ans, _ := IsCoprime(testCase.a, testCase.b)
+			if ans != testCase.y {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
 }
 
-// TestPrimeFactors tests the solution of P34.
-func TestPrimeFactors(t *testing.T) {
-  var testCases = []struct {
-    x int
-    y IntSlice
-  }{
-    {315, IntSlice{3, 3, 5, 7}},
-    {430, IntSlice{2, 5, 43}},
-    {728, IntSlice{2, 2, 2, 7, 13}},
-    {999, IntSlice{3, 3, 3, 37}},
-    {1725, IntSlice{3, 5, 5, 23}},
-  }
+// TestTotientPhi tests the solution of P34.
+func TestTotientPhi(t *testing.T) {
+	var testCases = []struct {
+		x int
+		y int
+	}{
+		{10, 4},
+		{17, 16},
+		{39, 24},
+		{198, 60},
+		{24420, 5760},
+	}
 
-  for _, testCase := range testCases {
-    testName := fmt.Sprint(testCase.x)
-    t.Run(testName, func(t *testing.T) {
-      ans := PrimeFactors(testCase.x)
-      if !(reflect.DeepEqual(ans, testCase.y)) {
-        t.Errorf("Expected %v but the result was %v", testCase.y, ans)
-      }
-    })
-  }
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.x)
+		t.Run(testName, func(t *testing.T) {
+			ans, _ := TotientPhi(testCase.x)
+			if ans != testCase.y {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
+}
+
+// TestPrimeFactors tests the solution of P35.
+func TestPrimeFactors(t *testing.T) {
+	var testCases = []struct {
+		x int
+		y IntSlice
+	}{
+		{315, IntSlice{3, 3, 5, 7}},
+		{430, IntSlice{2, 5, 43}},
+		{728, IntSlice{2, 2, 2, 7, 13}},
+		{999, IntSlice{3, 3, 3, 37}},
+		{1725, IntSlice{3, 5, 5, 23}},
+	}
+
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.x)
+		t.Run(testName, func(t *testing.T) {
+			ans := PrimeFactors(testCase.x)
+			if !(reflect.DeepEqual(ans, testCase.y)) {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
+}
+
+// TestPrimeFactorsMult tests the solution of P36.
+func TestPrimeFactorsMult(t *testing.T) {
+	var testCases = []struct {
+		x int
+		y []IntSlice
+	}{
+		{
+			315, []IntSlice{IntSlice{2, 3}, IntSlice{1, 5}, IntSlice{1, 7}},
+		},
+		{
+			430, []IntSlice{IntSlice{1, 2}, IntSlice{1, 5}, IntSlice{1, 43}},
+		},
+		{
+			728, []IntSlice{IntSlice{3, 2}, IntSlice{1, 7}, IntSlice{1, 13}},
+		},
+		{
+			999, []IntSlice{IntSlice{3, 3}, IntSlice{1, 37}},
+		},
+		{
+			1725, []IntSlice{IntSlice{1, 3}, IntSlice{2, 5}, IntSlice{1, 23}},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.x)
+		t.Run(testName, func(t *testing.T) {
+			ans := PrimeFactorsMult(testCase.x)
+			if !(reflect.DeepEqual(ans, testCase.y)) {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
+}
+
+// TestTotientPhi2 tests the solution of P37.
+func TestTotientPhi2(t *testing.T) {
+	var testCases = []struct {
+		x int
+		y int
+	}{
+		{10, 4},
+		{17, 16},
+		{39, 24},
+		{198, 60},
+		{24420, 5760},
+	}
+
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.x)
+		t.Run(testName, func(t *testing.T) {
+			ans, _ := TotientPhi2(testCase.x)
+			if ans != testCase.y {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
+}
+
+// TestRandomPermutation runs the solution of problem P38.
+func TestCompareTotien(t *testing.T) {
+	var testCases = []struct {
+		x int
+	}{
+		{
+			10,
+		},
+		{
+			50,
+		},
+	}
+
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.x)
+		t.Run(testName, func(t *testing.T) {
+			CompareTotient(testCase.x)
+		})
+	}
+}
+
+// TestPrimeFactors tests the solution of P39.
+func TestPrimeList(t *testing.T) {
+	var testCases = []struct {
+		lower int
+		upper int
+		y     IntSlice
+	}{
+		{9, 41, IntSlice{11, 13, 17, 19, 23, 29, 31, 37, 41}},
+		{3923, 4002, IntSlice{3923, 3929, 3931, 3943, 3947, 3967, 3989, 4001}},
+		{5003, 5024, IntSlice{5003, 5009, 5011, 5021, 5023}},
+		{283, 312, IntSlice{283, 293, 307, 311}},
+		{2131, 2142, IntSlice{2131, 2137, 2141}},
+	}
+
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.lower, testCase.upper)
+		t.Run(testName, func(t *testing.T) {
+			ans := PrimeList(testCase.lower, testCase.upper)
+			if !(reflect.DeepEqual(ans, testCase.y)) {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
+}
+
+// TestGoldbach tests the solution of P40.
+func TestGoldbach(t *testing.T) {
+	var testCases = []struct {
+		number int
+		y      IntSlice
+	}{
+		{28, IntSlice{5, 23}},
+		{30, IntSlice{7, 23}},
+		{1002, IntSlice{5, 997}},
+		{1004, IntSlice{7, 997}},
+		{10000, IntSlice{59, 9941}},
+		{5000, IntSlice{7, 4993}},
+	}
+
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.number)
+		t.Run(testName, func(t *testing.T) {
+			ans, _ := Goldbach(testCase.number)
+			if !(reflect.DeepEqual(ans, testCase.y)) {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
+}
+
+// TestGoldbach tests the solution of P41.
+func TestGoldbachList(t *testing.T) {
+	var testCases = []struct {
+		lower int
+		upper int
+		y     []IntSlice
+	}{
+		{
+			9, 20,
+			[]IntSlice{
+				IntSlice{3, 7}, IntSlice{5, 7}, IntSlice{3, 11},
+				IntSlice{3, 13}, IntSlice{5, 13}, IntSlice{3, 17},
+			},
+		},
+		{
+			28, 36,
+			[]IntSlice{
+				IntSlice{5, 23}, IntSlice{7, 23}, IntSlice{3, 29},
+				IntSlice{3, 31}, IntSlice{5, 31},
+			},
+		},
+		{
+			240, 246,
+			[]IntSlice{
+				IntSlice{7, 233}, IntSlice{3, 239}, IntSlice{3, 241}, IntSlice{5, 241}},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testName := fmt.Sprint(testCase.lower, testCase.upper)
+		t.Run(testName, func(t *testing.T) {
+			ans, _ := GoldbachList(testCase.lower, testCase.upper)
+			if !(reflect.DeepEqual(ans, testCase.y)) {
+				t.Errorf("Expected %v but the result was %v", testCase.y, ans)
+			}
+		})
+	}
 }
